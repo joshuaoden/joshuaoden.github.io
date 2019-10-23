5aa44bed0e12464da9eca8171080f30e84d024d0
joshuaoden <joshw.oden@gmail.com>
2019-10-23 13:17:22 -0500

    10/23 New profile and small updates

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
 {
     "particles": {
       "number": {
-        "value": 72,
+        "value": 120,
         "density": {
           "enable": true,
           "value_area": 881.8766334760375
